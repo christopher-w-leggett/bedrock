67fcce4b88d99beccf96254a89d5fce72a179855
Michael Hodgdon <mhodgdon@citytechinc.com>
2016-01-04 09:07:51 -0500

    fixes for links and tag injector

--- bedrock-core/src/main/groovy/com/citytechinc/aem/bedrock/core/specs/BedrockSpec.groovy
+++ bedrock-core/src/main/groovy/com/citytechinc/aem/bedrock/core/specs/BedrockSpec.groovy
@@ -8,7 +8,11 @@ import com.citytechinc.aem.prosper.mixins.JspTagMixin
 import com.citytechinc.aem.prosper.specs.ProsperSpec
 import com.citytechinc.aem.prosper.tag.JspTagProxy
 import com.day.cq.wcm.api.PageManager
+
 import org.apache.sling.api.adapter.AdapterFactory
+import org.apache.sling.api.resource.ResourceResolverFactory
+import org.apache.sling.testing.resourceresolver.MockResourceResolverFactory
+
 import spock.lang.Shared
 
 import javax.servlet.jsp.tagext.TagSupport
@@ -22,52 +26,50 @@ import static org.apache.sling.scripting.jsp.taglib.DefineObjectsTag.DEFAULT_RES
  */
 abstract class BedrockSpec extends ProsperSpec {
 
-    @Shared
-    PageManagerDecorator pageManagerDecorator
+	@Shared
+	PageManagerDecorator pageManagerDecorator
 
-    @Shared
-    JspTagMixin jspTag
+	@Shared
+	JspTagMixin jspTag
 
-    @Override
-    Collection<AdapterFactory> addAdapterFactories() {
-        [new BedrockAdapterFactory()]
-    }
+	@Override
+	Collection<AdapterFactory> addAdapterFactories() {
+		[new BedrockAdapterFactory()]
+	}
 
-    def setupSpec() {
-        pageManagerDecorator = resourceResolver.adaptTo(PageManagerDecorator)
-    }
+	def setupSpec() {
+		pageManagerDecorator = resourceResolver.adaptTo(PageManagerDecorator)
+	}
 
-    ComponentNode getComponentNode(String path) {
-        resourceResolver.getResource(path).adaptTo(ComponentNode)
-    }
+	ComponentNode getComponentNode(String path) {
+		resourceResolver.getResource(path).adaptTo(ComponentNode)
+	}
 
-    @Override
-    PageDecorator getPage(String path) {
-        pageManagerDecorator.getPage(path)
-    }
+	@Override
+	PageDecorator getPage(String path) {
+		pageManagerDecorator.getPage(path)
+	}
 
-    @Override
-    PageManagerDecorator getPageManager() {
-        pageManagerDecorator
-    }
+	@Override
+	PageManagerDecorator getPageManager() {
+		pageManagerDecorator
+	}
 
-    // delegate methods
+	// delegate methods
 
-    public <T extends TagSupport> JspTagProxy init(Class<T> tagClass, String path) {
-        init(tagClass, path, [:])
-    }
+	public <T extends TagSupport> JspTagProxy init(Class<T> tagClass, String path) {
+		init(tagClass, path, [:])
+	}
 
-    public <T extends TagSupport> JspTagProxy init(Class<T> tagClass, String path,
-        Map<String, Object> additionalPageContextAttributes) {
-        def resource = resourceResolver.getResource(path)
-        def currentPage = resourceResolver.adaptTo(PageManager).getContainingPage(resource)
+	public <T extends TagSupport> JspTagProxy init(Class<T> tagClass, String path,
+			Map<String, Object> additionalPageContextAttributes) {
+		def resource = resourceResolver.getResource(path)
+		def currentPage = resourceResolver.adaptTo(PageManager).getContainingPage(resource)
 
-        additionalPageContextAttributes[DEFAULT_RESOURCE_NAME] = resource
-        additionalPageContextAttributes[DEFAULT_CURRENT_PAGE_NAME] = currentPage
-        additionalPageContextAttributes[DEFAULT_BINDINGS_NAME] = bindingsBuilder.build {
-            setPath(path)
-        }
+		additionalPageContextAttributes[DEFAULT_RESOURCE_NAME] = resource
+		additionalPageContextAttributes[DEFAULT_CURRENT_PAGE_NAME] = currentPage
+		additionalPageContextAttributes[DEFAULT_BINDINGS_NAME] = bindingsBuilder.build { setPath(path) }
 
-        jspTag.init(tagClass, additionalPageContextAttributes)
-    }
+		jspTag.init(tagClass, additionalPageContextAttributes)
+	}
 }
